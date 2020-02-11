d244c85159e8e89bb30bed905c3be2b5dc55c02f
Damon Palovaara <dpalovaa@nmu.edu>
2020-02-11 16:12:52 -0500

    Got Client::prime() working

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 TODO:
 - Finish making this file
+- Separate Client, Server, and the unit tests into separate files
 
 Issues:
 - No way to handle jack buffer < udp_payload_size (if using small buffer or jumbo-frames)
--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -3,17 +3,18 @@ static TEST_ADDRESS: &str = "127.0.0.1";
 
 //extern crate jack;
 use std::net::UdpSocket;
+use std::thread;
 
 pub struct Server {
-    _sample_rate:     usize,        // Sample rate of JACK server
-    _num_channels:    usize,        // Number of channels to use
-    udp_payload_size: usize,        // Size of allocated space for each udp buffer
-    buffers:          Vec<Vec<u8>>, // Where the udp buffers reside
-    num_bufs:         usize,        // Number of udp buffers each channel splits into
-    send_address:     String,       // IP address and port in "xxx.xxx.xxx.xxx:xxxx" format
-    udp_socket:       UdpSocket,    // UDP socket that sends packets
-    count:            Vec<usize>,
-    key:              usize,
+    _sample_rate:  usize,        // Sample rate of JACK server
+    _num_channels: usize,        // Number of channels to use
+    payload_size:  usize,        // Size of allocated space for each udp buffer
+    buffers:       Vec<Vec<u8>>, // Where the udp buffers reside
+    num_bufs:      usize,        // Number of udp buffers each channel splits into
+    send_address:  String,       // IP address and port in "xxx.xxx.xxx.xxx:xxxx" format
+    udp_socket:    UdpSocket,    // UDP socket that sends packets
+    count:         Vec<usize>,   // Count used for generating header
+    key:           usize,        // Key used for generating header
 }
 
 impl Server {
@@ -26,19 +27,19 @@ impl Server {
         server_address: &str,
         send_address:   &str,
     ) -> Server {
-        let num_bufs         = ((jack_buf_size * 4) as f64 / (network_mtu - 100) as f64).ceil() as usize;
-        let udp_payload_size = ((jack_buf_size * 4) as f64 / num_bufs as f64).ceil() as usize + 4;
-        let raw_buf_size     = (num_bufs * 4) + (jack_buf_size * 4);
-        let buffers          = vec![vec![0; raw_buf_size]; _num_channels];
-        let udp_socket       = UdpSocket::bind(server_address).unwrap();
-        let send_address     = send_address.to_string();
-        let count            = vec![0; _num_channels];
-        let key              = (256 / num_bufs) * num_bufs;
+        let num_bufs     = ((jack_buf_size * 4) as f64 / (network_mtu - 100) as f64).ceil() as usize;
+        let payload_size = ((jack_buf_size * 4) as f64 / num_bufs as f64).ceil() as usize + 4;
+        let raw_buf_size = (num_bufs * 4) + (jack_buf_size * 4);
+        let buffers      = vec![vec![0; raw_buf_size]; _num_channels];
+        let udp_socket   = UdpSocket::bind(server_address).unwrap();
+        let send_address = send_address.to_string();
+        let count        = vec![0; _num_channels];
+        let key          = (256 / num_bufs) * num_bufs;
 
         Server {
             _sample_rate,
             _num_channels,
-            udp_payload_size,
+            payload_size,
             buffers,
             num_bufs,
             send_address,
@@ -51,13 +52,14 @@ impl Server {
     pub fn fill_buffer(&mut self, jack_buf: &[f32], channel_idx: usize) {
 
         // Convert jack buffer into bytes
-        let jack_buf_u8 = jack_buf_to_bytes(&jack_buf);
+        let jack_buf_u8 = f32_slice_to_bytes(&jack_buf);
 
         // Turn the buffers into chunk iterators
         let headers: Vec<[u8; 4]> = (0..self.num_bufs).map(|_| self.generate_header(channel_idx)).collect();
-        let mut udp_chunks        = self.buffers[channel_idx][..].chunks_mut(self.udp_payload_size);
-        let mut jack_chunks       = jack_buf_u8.chunks(self.udp_payload_size - 4);
+        let mut udp_chunks        = self.buffers[channel_idx][..].chunks_mut(self.payload_size);
+        let mut jack_chunks       = jack_buf_u8.chunks(self.payload_size - 4);
 
+        // Copy a header and jack chunk into each udp chunk
         for i in 0..self.num_bufs {
             let udp_chunk = udp_chunks.next().unwrap();
             udp_chunk[..4].copy_from_slice(&headers[i]);
@@ -68,7 +70,7 @@ impl Server {
     // Try to make this function async so Jack can update while this is sending
     pub fn send_packets(&self) {
         for i in 0..self.buffers.len() {
-            self.buffers[i].chunks(self.udp_payload_size).for_each(|udp_payload| {
+            self.buffers[i].chunks(self.payload_size).for_each(|udp_payload| {
                 self.udp_socket.send_to(udp_payload, &self.send_address).unwrap();
             });
         }
@@ -92,45 +94,57 @@ impl Server {
 
 }
 
-// Channel_number | Encoded index | Sample rate | number of chunks
 struct Client {
-    channel_number: usize, 
-    encoded_index:  usize,
     sample_rate:    usize,
-    num_chunks:     usize,
+    key:            usize,
+    jack_buf_size:  usize,
+    payload_size:   usize,
     server_address: String,
     server_socket:  UdpSocket,
 }
 
 impl Client {
-    fn new(
-        server_address: &str,
-    ) -> Client { 
-        let channel_number = 0;
-        let encoded_index = 0;
-        let sample_rate = 0;
-        let num_chunks = 0;
+    pub fn new(server_address: &str) -> Client { 
+        let sample_rate    = 0;
+        let key            = 0;
+        let jack_buf_size  = 0;
+        let payload_size   = 0;
         let server_address = server_address.to_string();
-        let server_socket = UdpSocket::bind(&server_address).unwrap();        
+        let server_socket  = UdpSocket::bind(&server_address).unwrap();        
 
         Client {
-            channel_number,
-            encoded_index,
             sample_rate,
-            num_chunks,
+            key,
+            jack_buf_size,
+            payload_size,
             server_address,
             server_socket,
         }
     }
 
-    fn read_packet(&mut self) {
+    pub fn read_packet(&mut self) {
         let mut buf = vec![0; 2000];
         self.server_socket.recv_from(&mut buf).unwrap();
+    }
+
+    pub fn prime(&mut self) {
+        let mut buf = vec![0; 2000];
+        let mut read_len;
+        loop {
+            read_len = self.server_socket.recv_from(&mut buf).unwrap().0;
+            if (buf[1] % buf[3]) != buf[3] - 1 || buf[3] == 1 {
+                break;
+            }
+        }
+        self.sample_rate   = buf[2] as usize;
+        self.key           = (256usize / buf[3] as usize) * buf[3] as usize;
+        self.jack_buf_size = 2usize.pow((( ((read_len - 4) * buf[3] as usize) / 4) as f64).log2().floor() as u32);
+        self.payload_size  = read_len - 4;
     }
 }
 
 // Casts a f32 slice as a u8 slice
-fn jack_buf_to_bytes(jack_buf: &[f32]) -> &[u8] {
+fn f32_slice_to_bytes(jack_buf: &[f32]) -> &[u8] {
     unsafe { 
         std::slice::from_raw_parts(jack_buf.as_ptr() as *const u8, jack_buf.len() * 4)
     }
@@ -160,7 +174,7 @@ mod tests {
             &send_address
         );
 
-        assert_eq!(server.udp_payload_size, 1370);
+        assert_eq!(server.payload_size, 1370);
         assert_eq!(server.buffers[0].len(), 4108);
         assert_eq!(server.key, 255);
     }
@@ -266,37 +280,43 @@ mod tests {
     }
 
     #[test]
-    fn test_server_client_handshake() {
+    fn test_client_prime() {
         // Create client
         let server_address = TEST_ADDRESS.to_owned() + ":9002";
         let mut client = Client::new(&server_address);
 
-        // Create server
-        let jack_buf_size  = 1024;
-        let _sample_rate   = 44100;
-        let _num_channels  = 2;
-        let network_mtu    = 8000;
-        let server_address = TEST_ADDRESS.to_owned() + ":8003";
-        let send_address   = TEST_ADDRESS.to_owned() + ":9002";
-        let mut server = Server::new(
-            jack_buf_size, 
-            _sample_rate, 
-            _num_channels,
-            network_mtu,
-            &server_address,
-            &send_address
-        );
+        // Create server on separate thread
+        thread::spawn(|| {
+            // Create server
+            let jack_buf_size  = 1024;
+            let _sample_rate   = 44100;
+            let _num_channels  = 2;
+            let network_mtu    = 1500;
+            let server_address = TEST_ADDRESS.to_owned() + ":8003";
+            let send_address   = TEST_ADDRESS.to_owned() + ":9002";
+            let mut server = Server::new(
+                jack_buf_size, 
+                _sample_rate, 
+                _num_channels,
+                network_mtu,
+                &server_address,
+                &send_address
+            );
+            // Fill server buffers
+            let ones = f32::from_be_bytes([1, 1, 1, 1]);
+            let jack_buffer = vec![ones; jack_buf_size]; 
+            server.fill_buffer(&jack_buffer, 0);
+            server.fill_buffer(&jack_buffer, 1);
 
-        // Fill server buffers
-        let ones = f32::from_be_bytes([1, 1, 1, 1]);
-        let jack_buffer = vec![ones; jack_buf_size]; 
-        server.fill_buffer(&jack_buffer, 0);
-        server.fill_buffer(&jack_buffer, 1);
+            // Send packets
+            loop { server.send_packets(); }
+        });   
 
-        // Send packets
-        server.send_packets();
-        
-        // Receive packet
-        client.read_packet();
+        client.prime();
+
+        assert_eq!(client.sample_rate, 0);
+        assert_eq!(client.key, 255);
+        assert_eq!(client.jack_buf_size, 1024);
+        assert_eq!(client.payload_size, 1366);
     }
 }
